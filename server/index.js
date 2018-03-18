17a52ad2ce95e9fc46687351576e2637af9daa61
JWKelly29 <jwkelly29@gmail.com>
2018-03-18 06:06:48 +0000

    runs on port contained in process.env.PORT or 5000. Added start script to package.json

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require("express");
-const app = express;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+console.log("Running on port: " + PORT);
+
+app.listen(PORT);
