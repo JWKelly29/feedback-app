91587ab75ab28c2f22616c7a25bb224da96ca0e5
JWKelly29 <jwkelly29@gmail.com>
2018-03-19 03:56:16 +0000

    created mongoose user model

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
-require("./services/passport.js");
+require("./services/passport");
+require("./models/user");
 
 mongoose.connect(keys.mongoURI);
 
