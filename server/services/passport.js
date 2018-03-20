46bb95009d227c8a1b980cd18771aad3f9430478
JWKelly29 <jwkelly29@gmail.com>
2018-03-20 22:48:39 +0000

    create new user if one doesn't exist in the database when google oauth returns user details

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
-      console.log("done: ", done);
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // User exists
+        } else {
+          // User does not exist, create new user
+          new User({ googleID: profile.id }).save();
+        }
+      });
     }
   )
 );
