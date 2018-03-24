ab357c53d1f6747e4909426afffc21b3554202cb
JWKelly29 <jwkelly29@gmail.com>
2018-03-24 00:47:00 +0000

    have to have .git file and package.json in root directory to successfully push to heroku. So I have initialized git within server

---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  // Making use of id assigned to user by mongo
+  // After user signs we want to save user id to cookie
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // user found
+          done(null, existingUser);
+        } else {
+          // no user found, create new user
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
