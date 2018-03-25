3ca4ca085e8e55ec043b3f0f7f2a00586c72268f
JWKelly29 <jwkelly29@gmail.com>
2018-03-26 00:53:49 +0100

    set up react router, used react router to display landing component on root path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2> Header </h2>;
+const Dashboard = () => <h2> Dashboard </h2>;
+const SurveryNew = () => <h2> Survey New </h2>;
+const Landing = () => <h2> Landing </h2>;
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
