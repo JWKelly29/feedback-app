302ed810012969aef05da8dc3ea556dfef93c126
JWKelly29 <jwkelly29@gmail.com>
2018-03-25 21:02:11 +0100

    refactored promises in passport.js

--- services/passport.js
+++ services/passport.js
@@ -25,18 +25,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // user found
-          done(null, existingUser);
-        } else {
-          // no user found, create new user
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        // user found
+        return done(null, existingUser);
+      }
+      // no user found, create new user
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
     }
   )
 );
