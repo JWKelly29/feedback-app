c58de700ced657c3ff1b9d2da7f87bf81dbcf72b
JWKelly29 <jwkelly29@gmail.com>
2018-03-27 20:24:14 +0100

    added switch statement to authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,12 @@
-export default function(state = {}, action) {
-  console.log(action);
+import FETCH_USER from "../actions/types";
+
+export default function(state = null, action) {
+  // returns null when waiting on actions
+  // returns payload when successfully fetched users
+  // returns false if there's no payload
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
