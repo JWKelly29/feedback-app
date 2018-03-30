be85a54d471b0e04797899902498d8d2c84740c6
JWKelly29 <jwkelly29@gmail.com>
2018-03-30 16:56:50 +0100

    Created stripe account, installed react-stripe-checkout, setup env variables

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
     "6183459130-4t51nh3c4ft8075vv1guus754vg6a2l1.apps.googleusercontent.com",
   googleClientSecret: "7ps8CpQHezlU_ecAl4i_L-FB",
   mongoURI: "mongodb://jack:Rangers121@ds119049.mlab.com:19049/emaily-dev",
-  cookieKey: "hdhpidvihiohii4940969uknkd3"
+  cookieKey: "hdhpidvihiohii4940969uknkd3",
+  stripePublishableKey: "pk_test_gJZEgJ732lu7hPCQBHQV31G4",
+  stripeSecretKey: "sk_test_RSD6RWCsNWu7BOyUsQ6VWklj"
 };
