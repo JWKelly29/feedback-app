967acbdf88cace290b0c4fba3a2df13aa0c3073d
JWKelly29 <jwkelly29@gmail.com>
2018-03-30 21:54:56 +0100

    installed and setup body-parser to parse incoming requests and assign it to req.body

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 require("./models/user");
 require("./services/passport");
 
@@ -10,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Middleware used on incoming request before they are sent off to request handlers
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 console.log("Running on port:  " + PORT);
