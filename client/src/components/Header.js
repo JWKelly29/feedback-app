4c495010508506fa296c36f14b8aa486604f4c63
JWKelly29 <jwkelly29@gmail.com>
2018-03-30 18:43:53 +0100

    payments component added, strip intial setup completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -9,17 +10,20 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return (
+        return [
           <li>
             <a href="/auth/google"> Login with Google </a>
           </li>
-        );
+        ];
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="/api/logout"> Logout </a>
           </li>
-        );
+        ];
     }
   }
 
