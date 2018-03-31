0559bc5cd189543e17817bd4c5b00c80bc1549a6
JWKelly29 <jwkelly29@gmail.com>
2018-03-31 22:18:27 +0100

    created requireCredits middleware function

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 // called next to indicate it is going to be passed on to other middle wares before request is handled
 module.exports = (req, res, next) => {
-  if (!user) {
+  if (!req.user) {
     return res.status(401).send({ error: "User must be logged in!" });
   }
 
