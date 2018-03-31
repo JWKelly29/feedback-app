f6c2432a4e48a8c1cd295707cf9706d20c18918a
JWKelly29 <jwkelly29@gmail.com>
2018-03-31 18:10:34 +0100

    handled routing in production

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV == "production") {
+  // Express to serve production assets e.g. main.js or main.css
+  // if route comes in that express doesn't understand look in our build directory
+  app.use(express.static("client/build"));
+
+  // Express to serve index.html if it doesn't recognise the routes
+
+  // this is route handler to catch any file names express doesnt understand. So
+  // first it will look in express route handlers defined above, then in build, if still can't find anything
+  // serve index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 console.log("Running on port:  " + PORT);
 
