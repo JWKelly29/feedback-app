3af8584bacd0526a0e3794e17917eed9f87ad91b
JWKelly29 <jwkelly29@gmail.com>
2018-04-04 22:04:57 +0100

    Added validation function to surveyForm and handled my first validation error. Also styled the error messaged and included an icon.

--- client/src/components/surveys/surveyField.js
+++ client/src/components/surveys/surveyField.js
@@ -1,10 +1,34 @@
 // Survey field contains logic to render label and text input
 import React from "react";
 
-export default ({ input, label }) => {
+export default ({ input, label, meta }) => {
+  console.log(meta);
   return (
     <div>
-      <label>{label}</label>
+      <div style={{ marginTop: "2%" }}>
+        <label style={{ display: "inline-block", textAlign: "center" }}>
+          {label}
+        </label>
+        <div
+          className="red-text"
+          style={{
+            display: "inline-block",
+            marginLeft: "10%",
+            lineHeight: "10px"
+          }}
+        >
+          {meta.touched &&
+            meta.error && (
+              <i
+                className="material-icons"
+                style={{ verticalAlign: "text-bottom", marginRight: "5px" }}
+              >
+                error_outline
+              </i>
+            )}
+          {meta.touched && meta.error}
+        </div>
+      </div>
       <input {...input} />
     </div>
   );
