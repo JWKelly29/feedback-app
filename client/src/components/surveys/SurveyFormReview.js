bbd46d38a9123ed6fc336a9318bbb96c04c5ae47
JWKelly29 <jwkelly29@gmail.com>
2018-04-05 22:31:38 +0100

    added form review component with a back button. Also added state to survey new to control visibility

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SurveyFormReview = ({ onCancel }) => {
+  return (
+    <div>
+      <h5>Please Review and Confirm Your Entries</h5>
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
+    </div>
+  );
+};
+
+export default SurveyFormReview;
