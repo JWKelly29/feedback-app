30c279ed72ad37cbdf4a1d8e4ab53fad733faf3b
JWKelly29 <jwkelly29@gmail.com>
2018-04-10 00:32:03 +0100

    created jsx and styling for surveyList component

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -4,11 +4,29 @@ import { fetchSurveys } from "../../actions";
 
 class SurveyList extends Component {
   componentDidMount() {
-    this.props.fetchSurveys;
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p> Sent on {new Date(survey.dateSent).toLocaleDateString()}</p>
+          </div>
+          <div className="card-action">
+            <a> Yes: {survey.yes}</a>
+            <a> No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
   }
 
   render() {
-    return <div>Survey List</div>;
+    return <div> {this.renderSurveys()} </div>;
   }
 }
 
